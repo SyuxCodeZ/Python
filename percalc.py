52f947bf7a3b748488fb1309c401ad2edd1b051c
SyuxCodeZ <cuberdytm@gmail.com>
2024-12-21 19:49:59 +0600

    calculator

--- percalc.py
+++ percalc.py
@@ -1,19 +1,47 @@
+# the inputs
+
 operator = input("Enter An Operator (+ - * /): ")
 
 num1 = float(input("Enter The 1st Number: "))
+
 num2 = float(input("Enter The 2nd Number: "))
 
+# the results
+
 if operator == "+":
+
     result = num1 * num2
-    print(round(result, 4))
+
+    result = int(result)
+
+    print(result)
+
 elif operator == "-":
+
     result = num1 - num2
-    print(round(result, 4))
+
+    result = int(result)
+
+    print(result)
+
 elif operator == "*":
+
     result = num1 * num2
-    print(round(result, 4))
+
+    result = int(result)
+
+    print(result)
+
 elif operator == "/":
+
     result = num1 / num2
-    print(round(result, 4))
+
+    result = int(result)
+
+    print(result)
+
+#the errror message
+
 else:
+
     print(f"{operator} Is Not A Valid Operator")
